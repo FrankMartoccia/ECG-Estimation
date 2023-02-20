3f2030b232e80e59815288c15a1a03424f1ea859
Salvatore Lombardi <salvatore.lombardi29@gmail.com>
2023-02-21 00:34:19 +0100

    Changed argument name

--- extractFeatures.m
+++ extractFeatures.m
@@ -21,53 +21,53 @@ end
 
 %end
 
-function [SignalFeatures] = computeFeatures(Timeseries)
+function [Features] = computeFeatures(TimeseriesMatrix)
     % Computes 14 features from a given signal
 
     % Feature 1: Mean
-    mean_signal = mean(Timeseries);
+    mean_signal = mean(TimeseriesMatrix);
 
     % Feature 2: Standard deviation
-    std_signal = std(Timeseries);
+    std_signal = std(TimeseriesMatrix);
 
     % Feature 3: Maximum amplitude
-    max_amp = max(Timeseries);
+    max_amp = max(TimeseriesMatrix);
 
     % Feature 4: Minimum amplitude
-    min_amp = min(Timeseries);
+    min_amp = min(TimeseriesMatrix);
 
     % Feature 5: Median
-    median_signal = median(Timeseries);
+    median_signal = median(TimeseriesMatrix);
 
     % Feature 6: Interquartile range
-    IQR_signal = iqr(Timeseries);
+    IQR_signal = iqr(TimeseriesMatrix);
 
     % Feature 7: Skewness
-    skewness_signal = skewness(Timeseries);
+    skewness_signal = skewness(TimeseriesMatrix);
 
     % Feature 8: Kurtosis
-    kurtosis_signal = kurtosis(Timeseries);
+    kurtosis_signal = kurtosis(TimeseriesMatrix);
 
     % Feature 9: Mean absolute deviation
-    mad_signal = mad(Timeseries);
+    mad_signal = mad(TimeseriesMatrix);
 
     % Feature 10: Root mean square
-    rms_signal = rms(Timeseries);
+    rms_signal = rms(TimeseriesMatrix);
 
     % Feature 11: Peak to peak amplitude
-    peak_to_peak_amp = peak2peak(Timeseries);
+    peak_to_peak_amp = peak2peak(TimeseriesMatrix);
 
     % Feature 12: Waveform length
-    waveform_length = sum(abs(diff(Timeseries)));
+    waveform_length = sum(abs(diff(TimeseriesMatrix)));
 
     % Feature 13: Power spectral density
-    [pxx, ~] = pwelch(Timeseries);
+    [pxx, ~] = pwelch(TimeseriesMatrix);
     psd = bandpower(pxx);
 
     % Store features in a matrix
-    SignalFeatures = [mean_signal; std_signal; max_amp; min_amp; median_signal;...
+    Features = [mean_signal; std_signal; max_amp; min_amp; median_signal;...
                 IQR_signal; skewness_signal; kurtosis_signal;...
                 mad_signal; rms_signal; peak_to_peak_amp; waveform_length; psd];
-    SignalFeatures = SignalFeatures(:)';
+    Features = Features(:)';
 end
 
