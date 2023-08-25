3aba2aea964b374d3ad6dc4f7932f5a03d502df3
Salvatore Lombardi <74710148+salbh@users.noreply.github.com>
2023-08-25 13:20:29 +0200

    Creation of MLPs for mean and std

---
+++ task-3.1/MLP/MLPMean.m
@@ -0,0 +1,24 @@
+% Workspace cleaning
+clear
+close all
+clc
+
+load("data/afterFeaturesSelection.mat");
+
+% Network parameters
+hiddenSizes = 60;
+trainFcn = 'trainlm';
+
+% Network creation
+net = fitnet(hiddenSizes, trainFcn);
+net.divideParam.trainRatio = 0.7;
+net.divideParam.valRatio = 0.1;
+net.divideParam.testRatio = 0.2;
+net.performFcn = 'mse'; 
+
+[net,tr] = train(net, InputMeanECG, TargetMeanECG);
+
+y = net(InputMeanECG);
+
+% Plot creation
+figure, plotregression(TargetMeanECG, y)
---
+++ task-3.1/MLP/MLPStd.m
@@ -0,0 +1,24 @@
+% Workspace cleaning
+clear
+close all
+clc
+
+load("data/afterFeaturesSelection.mat");
+
+% Network parameters
+hiddenSizes = 65;
+trainFcn = 'trainlm';
+
+% Network creation
+net = fitnet(hiddenSizes, trainFcn);
+net.divideParam.trainRatio = 0.7;
+net.divideParam.valRatio = 0.1;
+net.divideParam.testRatio = 0.2;
+net.performFcn = 'mse'; 
+
+[net,tr] = train(net, InputStdECG, TargetStdECG);
+
+y = net(InputStdECG);
+
+% Plot creation
+figure, plotregression(TargetStdECG, y)
