825cd92853eb5c5f38065018b7f95691ff5537f1
Francesco Martoccia <92721828+FrankMartoccia@users.noreply.github.com>
2023-08-29 16:22:51 +0200

    Update extractFeaturesFIS.m

--- dataPreparation/extractFeaturesFIS.m
+++ dataPreparation/extractFeaturesFIS.m
@@ -9,15 +9,18 @@ load('data/afterFeaturesSelection.mat')
 options = statset('Display', 'iter','UseParallel',true);
 
 % Running features selection to determine top 3 features for Fuzzy System
-selectFeatures(InputActivity, TargetActivityClassesVec, options, 'Activity', 3)
+selectedFeatures = selectFeatures(InputActivity, TargetActivityClassesVec, options, 'Activity', 3);
+
+% Save results
+InputActivity = InputActivity(selectedFeatures, :);
+save('data/inputFuzzySystem', InputActivity, TargetActivityClassesVec);
 
 % Run feature selection and save the results
-function selectFeatures(Features, Target, options, fileName, nFeatures)
+function [selectedFeatures] = selectFeatures(Features, Target, options, fileName, nFeatures)
     filePath = (strcat('results/dataPreparation/sf', fileName));
     diary(strcat(filePath, 'Log.txt'));
     selectedFeatures = sequentialfs(@selectionCriterionActivity, Features', Target', ...
         'options', options, 'nfeatures', nFeatures);
     diary('off');
-    saveBestFeatures(filePath, selectedFeatures);
 end
 
