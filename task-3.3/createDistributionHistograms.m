ec397b81ba3b309982e4f7e0f962ac82db5f72a1
Salvatore Lombardi <74710148+salbh@users.noreply.github.com>
2023-08-29 18:25:17 +0200

    Added histograms creation

---
+++ task-3.3/createDistributionHistograms.m
@@ -0,0 +1,46 @@
+% Load data
+clear; clc; close all;
+load('data/inputFuzzySystem.mat');
+
+t = vec2ind(TargetActivityClassesVec);
+
+% Divide input in classes
+sit = InputActivity(:, t == 1);
+walk = InputActivity(:, t == 2);
+run = InputActivity(:, t == 3);
+
+nFeatures = size(InputActivity, 1);
+nBins = 15;
+
+% Plot feature distribution to define membership_function
+figure;
+for i = 1:nFeatures
+    subplot(2, 2, i);
+    plotHistogram(InputActivity(i, :), "All Activities", i, nBins);
+end
+sgtitle("Feature Distribution for All Activities");
+
+% Plot histograms to define rules
+figure;
+for i = 1:nFeatures
+    subplot(nFeatures, 3, 3 * (i - 1) + 1);
+    plotHistogram(sit(i, :), "SIT", i, nBins);
+    
+    subplot(nFeatures, 3, 3 * (i - 1) + 2);
+    plotHistogram(walk(i, :), "WALK", i, nBins);
+    
+    subplot(nFeatures, 3, 3 * (i - 1) + 3);
+    plotHistogram(run(i, :), "RUN", i, nBins);
+end
+sgtitle("Feature Distribution by Activity");
+
+% Adjust figure layout
+%figureHandle = gcf;
+%set(findall(figureHandle, 'Type', 'Axes'), 'YScale', 'log');
+
+% Define function to plot histogram
+function plotHistogram(data, classLabel, featureIndex, nBins)
+    histogram(data, nBins, 'BinWidth', 0.05, 'BinLimits', [0, 1]);
+    title("Histogram of feature " + num2str(featureIndex) + ": " + classLabel);
+    xticks(linspace(0, 1, 11));
+end
