3263967ac2afe343aaf19253e4f7f1ec056abdde
Salvatore Lombardi <74710148+salbh@users.noreply.github.com>
2023-10-09 12:20:02 +0200

    Fixed plot

--- task-4.3/multiStepForecasting.m
+++ task-4.3/multiStepForecasting.m
@@ -3,7 +3,7 @@ clear; clc; close all;
 load('data/inputMultiStepRNN', 'net', 'ValidationData', 'PrevisionTarget');
 
 % Initialize a matrix to store RMSE for each test sequence
-RmseMatrix = nan(length(ValidationData), 1);
+RmseVector = nan(length(ValidationData), 1);
 
 % Initialize a cell array to store predicted values for each sequence
 PredictedValuesCell = cell(length(ValidationData), 1);
@@ -33,22 +33,24 @@ for windowIdx = 1:length(ValidationData)
 
     % Calculate the RMSE for this sequence
     error = (PredictedValues - TrueTargets);
-    RmseMatrix(windowIdx) = sqrt(mean(error.^2));
+    RmseVector(windowIdx) = sqrt(mean(error.^2));
 
     % Display progress every 500 sequences
     if mod(windowIdx, 500) == 0
-        disp(['Processed sequence ', num2str(windowIdx), ...
+        disp(['Processed sequences: ', num2str(windowIdx), ...
             ' - ', num2str(round(windowIdx/length(ValidationData) * 100)), '%']); 
     end
 end
 
 % Calculate the overall RMSE
-overallRMSE = mean(RmseMatrix(:));
+overallRMSE = mean(RmseVector(:));
 fprintf('Overall RMSE: %.4f\n', overallRMSE);
+
+save('data/afterMultiStepForecasting', "RmseVector", "overallRMSE", "PredictedValuesCell");
 
 % Plot RMSE for each sequence
 figure;
-plot(1:length(ValidationData), RmseMatrix, '.');
+plot(1:length(ValidationData), RmseVector, '.');
 xlabel('Sequence Index');
 ylabel('RMSE');
 title('RMSE for Each Sequence');
@@ -56,7 +58,7 @@ grid on;
 
 % Plot histogram of RMSE values
 figure;
-histogram(RmseMatrix(:), 50);
+histogram(RmseVector(:), 50);
 xlabel('RMSE');
 ylabel('Frequency');
 title('Histogram of RMSE Values');
@@ -65,23 +67,18 @@ grid on;
 % Choose a specific sequence for the single-channel plot
 sequenceToPlot = 1; % Change to the desired sequence index
 
-% Plot the input data, true values, and predicted values
+% Extract relevant data for the chosen sequence
+trueValues = PrevisionTarget{sequenceToPlot};
+predictedValues = PredictedValuesCell{sequenceToPlot};
+
+% Plot the true values and predicted values
 figure;
-plot(1:length(ValidationData{sequenceToPlot}), ValidationData{sequenceToPlot}, ...
-    'b', 'LineWidth', 1.5);
-
+plot(1:length(trueValues), trueValues, 'g', 'LineWidth', 1.5);
 hold on;
-
-plot(length(ValidationData{sequenceToPlot}):length(ValidationData{sequenceToPlot}) ...
-    + length(PrevisionTarget{sequenceToPlot}) - 1, PrevisionTarget{sequenceToPlot}, ...
-    'g', 'LineWidth', 1.5);
-
-plot(length(ValidationData{sequenceToPlot}):length(ValidationData{sequenceToPlot}) ...
-    + length(PrevisionTarget{sequenceToPlot}) - 1, PredictedValuesCell{sequenceToPlot}, ...
-    'r.', 'MarkerSize', 10);
+plot(1:length(predictedValues), predictedValues, 'r', 'LineWidth', 1.5);
 
 xlabel('Time Step');
 ylabel('Value');
-title(['Example of Prediction for Sequence ' num2str(sequenceToPlot)]);
-legend('Input Data', 'True Values', 'Predicted Values');
+title(['Comparison of True Values and Predicted Values for Sequence ' num2str(sequenceToPlot)]);
+legend('True Values', 'Predicted Values');
 grid on;
